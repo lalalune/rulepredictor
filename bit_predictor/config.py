e2b7f253073201afe2c5c666eb65cb3e04d84f8a
moon <shawmakesmagic@gmail.com>
2024-06-22 23:19:41 -0700

    cray batch size

--- bit_predictor/config.py
+++ bit_predictor/config.py
@@ -5,7 +5,7 @@ import torch
 
 batch_size = 2
 if torch.cuda.is_available():
-    batch_size = 1024
+    batch_size = 2048
 d_model = 32
 nhead = 4
 num_layers = 6
@@ -13,6 +13,6 @@ dim_feedforward = 128
 max_seq_length = 72
 max_context_length = 64
 max_prediction_length = 8
-dropout_rate = 0.2
+dropout_rate = 0.12
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 checkpoint_path = Path("bit_checkpoint.pt")
